6043425d024877054010d8d1ef4dd9a32a7c3d13
AlejoVinluan <alejo.vinluan146@gmail.com>
2022-09-28 13:44:53 -0500

    Added base files, very basic Memory setup

--- Processor.java
+++ Processor.java
@@ -1,5 +1,15 @@
 package Project1;
 
 public class Processor {
-    
+
+    int PC, SP, IR, AC, X, Y;
+
+    public static void main(String[] args){
+        // Check that a file was given in args[0]
+        if(args.length < 1){
+            throw new Error("Not enough arguments. Please specify user file.");
+        }
+
+
+    }
 }
