c72ea8bfe63019c7c2a5b4d96c14a834137cec22
AlejoVinluan <alejo.vinluan146@gmail.com>
2022-10-01 21:29:15 -0500

    A lot of progress. Saving to Github for now

--- Memory.java
+++ Memory.java
@@ -1,32 +1,117 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/*
+ * NOTES:
+ *  1) When memory object is made, Memory object is created.
+ *    a) Memory object will take input file and write it to user memory.
+ * 2) Memory will then wait for inputs coming from processor
+ */
 
 public class Memory {
     static int[] memory;
-    static int userCount;
+    static int memoryCount;
+    static String fileName;
 
-    public Memory(){
+    public static void loadMemory() throws FileNotFoundException{
+        /*
+         * Initialize memory of size 2000
+         *  index 0-999 for user program
+         *  1000-1999 are for system
+         */
         memory = new int[2000];
-        userCount = 0;
-    }
+        // Pointer to local index.
+        memoryCount = 0;
 
-    public static void write(int val){
-        memory[userCount] = val;
-        userCount++;
-    }
-
-    public static void main(String[] args){
         /*
          * When memory class is initialized, 
          *   it reads incoming stream from Processor.java 
          */ 
-        Scanner sc = new Scanner(System.in);
-        if(sc.hasNext()){
+        File file = new File(fileName);
+        Scanner sc = new Scanner(file);
+        while(sc.hasNextLine()){
             // Get command integer while ignoring comments
-            int command = Integer.parseInt(sc.nextLine().split(" ")[0]);
+            String command = sc.nextLine();
+            command.trim();
 
-            // Write each command into memory (located 0-999)
-            write(command);
+            // Check for empty line
+            if(command.length() <= 0){
+                continue;
+            }
+
+            // Change command so that we only access the first value
+            command = command.split(" ")[0];
+
+            // Check if line starts with a comment
+            if(command.charAt(0) == '/'){
+                continue;
+            }
+
+            // If the command starts with a period, change index
+            if(command.charAt(0) == '.'){
+                String newIndex = command.substring(1,command.length()-1);
+                memoryCount = Integer.parseInt(newIndex);
+                continue;
+            }
+    
+            // Write to memory
+            memory[memoryCount] = Integer.parseInt(command);
+            memoryCount++;
         }
+        sc.close();
+    }
+
+    public static int read(int index){
+        // Check if user is attempting to access system data.
+        return memory[index];
+    }
+
+    public static void write(int index, int val){
+        // Check if user wants to write to system code.
+        memory[memoryCount] = val;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException{
+        if(args.length < 1){
+            throw new Error("Input file not provided.");
+        }
+        fileName = args[0];
+        loadMemory();
+
+        // Read incoming stream from Processor.java
+        Scanner sc = new Scanner(System.in);
+
+        boolean running = true;
+        while(running){
+            /*
+             * Listen to input from Processor.java and execute commands
+             *  0 - read
+             *  1 - write
+             *  2 - end
+             * 
+             * Commands from Processor.java seperated by commas
+             *  - command[0] - command
+             *  - command[1] - index
+             *  - command[2] - value
+             * 
+             *  If command[index] is not used, we will send -1 instead to prevent error
+             *  (for example, read does not use command[2], so -1 will be sent)
+             */
+            String[] command_whole = sc.nextLine().split(",");
+            int command = Integer.parseInt(command_whole[0]);
+            int index = Integer.parseInt(command_whole[1]);
+            int value = Integer.parseInt(command_whole[2]);
+            if(command == 0){
+                System.out.println(read(index));
+            }   else if (command == 1){
+                write(index,value);
+            }   else if (command == 2){
+                running = false;
+            }
+        }
+        sc.close();
+        System.exit(0);
     }
 
 }
--- Processor.java
+++ Processor.java
@@ -1,15 +1,179 @@
 import java.io.*;
 import java.lang.Runtime;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Processor {
 
-    int PC, SP, IR, AC, X, Y;
+    /*
+     * 1) Program counter fetches instruction from memory
+     * 2) Execute carries out instruction
+     * 3) After fetch, program counter is incremented
+     * 4) Fetched instruction loads into IR
+     * 5) Instruction in IR is decoded
+     */
+
+    /*
+     * CPU registers
+     *  PC - program counter (counts where we are in program)
+     *  SP - 
+     *  IR - instruction register (fetches )
+     *  AC -
+     *  X - 
+     *  Y - 
+     */
+    static int PC, SP, IR, AC, X, Y;
+    // Timer
+    static int timer;
+    // Input Stream will get inputs from Memory process
+    static InputStream is;
+    // Output Stream will output to Memory process
+    static OutputStream os;
+    // Print Writer used to write to memory file
+    static PrintWriter pw;
+    // Scanner to listen for input coming from Memory
+    static Scanner sc;
+
+    // Reads from memory table
+    public static int readMemory(int index){
+        // Check if user is attempting to access kernel memory
+        if(index > 999){
+            throw new Error("Unable to access system memory.");
+        }
+        // Print command of "read" and index
+        //  - [0,indexRead,-1(default)]
+        pw.printf("0,%d,-1",index);
+        pw.flush();
+        // Returns what was in the given index
+        return Integer.parseInt(sc.nextLine());
+    }
+
+    public static void writeMemory(int index, int value){
+        // Check if user is attempting to access kernel memory
+        if(index > 999){
+            throw new Error("Unable to write to system memory.");
+        }
+        // Print command of "write", index, and value
+        // - [1,indexModified,valueStored]
+        pw.printf("1,%d,%d",index,value);
+        pw.flush();
+    }
+
+    // Read current PC (program counter)
+    public static void fetch(){
+        // Fetch will read next instruction in PC
+        IR = readMemory(PC);
+        PC++;
+    }
+
+    public static void execute(){
+        switch(IR){
+            // Load value into AC
+            case 1:
+                fetch();
+                AC = IR;
+                break;
+            // Load value at address into AC
+            case 2:
+                fetch();
+                AC = readMemory(IR);
+                break;
+            // Load value from address in given address into AC
+            case 3:
+                fetch();
+                int temp = readMemory(IR);
+                AC = readMemory(temp);
+                break;
+            // Load value at address + x into AC
+            case 4:
+                fetch();
+                AC = readMemory(IR + X);
+                break;
+            // Load value address + y into AC
+            case 5:
+                fetch();
+                AC = readMemory(IR + Y);
+                break;
+            // Loads adderss SP + x into AC
+            case 6:
+                AC = readMemory(SP + X);
+                break;
+            // Store AC into address
+            case 7:
+                fetch();
+                writeMemory(IR,AC);
+                break;
+            case 8:
+                Random rand = new Random();
+                int randomInt = rand.nextInt(100)+1;
+                AC = randomInt;
+                break;
+            case 9: 
+                fetch();
+                if(IR == 1)
+                    System.out.print(AC);
+                if (IR == 2)
+                    System.out.print((char)AC);
+                break;
+            case 10:
+                AC += X;
+                break;
+            case 11:
+                AC += Y;
+                break;
+            case 12:
+                AC -= X;
+                break;
+            case 13:
+                AC -= Y;
+                break;
+            case 14:
+                X = AC;
+                break;
+            case 15:
+                AC = X;
+                break;
+            case 16:
+                Y = AC;
+                break;
+            case 17:
+                AC = Y;
+                break;
+            case 18:
+                SP = AC;
+                break;
+            case 19:
+                AC = SP;
+                break;
+            case 20:
+                fetch();
+                PC = IR;
+                break;
+            case 21:
+                fetch();
+                if(AC == 0)
+                    PC = IR;
+                break;
+            case 22:
+                fetch();
+                if(AC != 0)
+                    PC = IR;
+                break;
+            case 23:
+                break;
+
+        }
+    }
 
     public static void main(String[] args) throws IOException{
         // Check that a file was given in args[0]
-        if(args.length < 1){
-            throw new Error("Not enough arguments. Please specify user file.");
+        if(args.length < 2){
+            throw new Error("Not enough arguments. Please specify user file and timer.");
         }
+
+        // Set time to 0 and timer to 
+        int time = 0;
+        timer = Integer.parseInt(args[1]);
 
         // Runtime will allow us to execute OS commands
         Runtime rt = Runtime.getRuntime();
@@ -17,9 +181,24 @@ public class Processor {
         //   Will also be used to pass in initial input file to memory
         Process proc = rt.exec("java Memory " + args[0]);
         // Input Stream will get inputs from Memory process
-        InputStream is = proc.getInputStream();
+        is = proc.getInputStream();
         // Output Stream will output to Memory process
-        OutputStream os = proc.getOutputStream();
+        os = proc.getOutputStream();
+        // Used to write to memory file
+        pw = new PrintWriter(os);
+        // Used to listen for input from memory file
+        sc = new Scanner(proc.getInputStream());
+
+        // Begin running Processor
+        boolean running = true;
+        PC = 0;
+
+        while(running || time < timer + 1){
+            // fetch instruction and store in IR
+            fetch();
+            // execute instruction in IR
+            execute();
+        }
 
 
 
